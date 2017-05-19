fbb3b53629dca71498b3696c213f7ef6deb623be
rougewood <roger.zhangyu@gmail.com>
2017-05-19 11:44:27 -0400

    update

--- scripts/inflectionPoint.R
+++ scripts/inflectionPoint.R
@@ -6,9 +6,14 @@ xl <- seq(min(x),max(x), (max(x) - min(x))/1000)
 out = predict(lo,xl)
 lines(xl, out, col='red', lwd=2)
 
-infl <- c(FALSE, diff(diff(out)>0)!=0)
+infl <- c(FALSE, diff(diff(out)>0)>0)
 print(diff(out))
 points(xl[infl ], out[infl ], col="blue")
+
+infl <- c(FALSE, diff(diff(out)>0)<0)
+print(diff(out))
+points(xl[infl ], out[infl ], col="red")
+
 
 df.Trades <- read.csv(file="~/R-Research/data/ES_Sample/ES_Trades.csv", nrows=8000)
 names <- colnames(df.Trades)
@@ -30,10 +35,13 @@ out1 = predict(lo, t)
 
 lines(t, out1, col='red', lwd=2)
 
-infl <- c(FALSE, diff(diff(out1)>0)!=0)
+infl <- c(FALSE, diff(diff(out1)>0)>0)
 print(diff(out1))
 points(t[infl ], out1[infl ], col="blue")
 
+infl <- c(FALSE, diff(diff(out1)>0)<0)
+print(diff(out1))
+points(t[infl ], out1[infl ], col="red")
 
 colMax <- function(data) sapply(data, max, na.rm = TRUE)
 
