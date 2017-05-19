5fcfd8f40cbd7c55fa95c41efa8d51e515faf92c
rougewood <roger.zhangyu@gmail.com>
2017-05-19 17:07:27 -0400

    add macd

--- scripts/inflectionPoint.R
+++ scripts/inflectionPoint.R
@@ -14,23 +14,25 @@ infl <- c(FALSE, diff(diff(out)>0)<0)
 print(diff(out))
 points(xl[infl ], out[infl ], col="red")
 
+rows = 150000
 
-df.Trades <- read.csv(file="~/R-Research/data/ES_Sample/ES_Trades.csv", nrows=8000)
+df.Trades <- read.csv(file="~/R-Research/data/ES_Sample/ES_Trades.csv", nrows=rows)
 names <- colnames(df.Trades)
 
-#df.Trades <- read.csv(file="~/R-Research/data/ES_Sample/ES_Trades.csv", nrows=2500)
-#df.Trades <- read.csv(file="~/R-Research/data/ES_Sample/ES_Trades.csv", skip=2000, nrows=2500)
-#df.Trades <- read.csv(file="~/R-Research/data/ES_Sample/ES_Trades.csv", skip=4000, nrows=2500)
-#df.Trades <- read.csv(file="~/R-Research/data/ES_Sample/ES_Trades.csv", skip=6000, nrows=2500)
+#df.Trades <- read.csv(file="~/R-Research/data/ES_Sample/ES_Trades.csv", nrows=rows)
+#df.Trades <- read.csv(file="~/R-Research/data/ES_Sample/ES_Trades.csv", skip=8000, nrows=rows)
+#df.Trades <- read.csv(file="~/R-Research/data/ES_Sample/ES_Trades.csv", skip=80000, nrows=rows)
+df.Trades <- read.csv(file="~/R-Research/data/ES_Sample/ES_Trades.csv", skip=120000, nrows=rows)
+df.Trades
 
 colnames(df.Trades) <- names
 #names <- colnames(df.Trades)
 p = c(df.Trades$Price)
-t = seq(1, 8000, 1)
+t = seq(1, rows, 1)
 
 plot(t,p)
 #lo <- nls(p~a + b*t^2, start = list(a = 0.12345, b = 0.54321))
-lo <- loess(p~t,span=1, degree=2)
+lo <- loess(p~t,span=0.2, degree=2)
 out1 = predict(lo, t)
 
 lines(t, out1, col='red', lwd=2)
@@ -41,7 +43,11 @@ points(t[infl ], out1[infl ], col="blue")
 
 infl <- c(FALSE, diff(diff(out1)>0)<0)
 print(diff(out1))
-points(t[infl ], out1[infl ], col="red")
+points(t[infl ], out1[infl ], col="red", type="b")
 
 colMax <- function(data) sapply(data, max, na.rm = TRUE)
 
+install.packages("quantmod")
+library(quantmod)
+chartSeries(out1)
+addMACD()
